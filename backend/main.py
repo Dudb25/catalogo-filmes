fd67a7892991773a870a944c4cc498ac77330a2e
Dudb25 <edudomingos0@gmail.com>
2026-05-22 23:37:56 -0300

    main.py e teste de rotas

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,11 @@
-print("Teste da branch")
+from fastapi import FastAPI
+from routers import filmes, users
+
+app = FastAPI()
+
+app.include_router(users.router)
+app.include_router(filmes.router)
+
+@app.get("/")
+def home():
+    return {"mensagem": "API do catálogo de filmes funcionando"}
