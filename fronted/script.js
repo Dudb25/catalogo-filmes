995959d8d995404592d78e7689012addb1aa7a6a
Marcus Vinicius <marcusdevinicius027@gmail.com>
2026-05-27 00:19:08 -0300

    Implementando função sobre o botão do formulário para a captação dos valores preenchidos e envia-los para o backend.

---
+++ fronted/script.js
@@ -0,0 +1,36 @@
+$("form").on("submit", function(event) {
+
+    event.preventDefault();
+
+    const titulo = $("#titulo").val();
+    const genero = $("#genero").val();
+    const status = $("#status").val();
+    const nota = $("#nota").val();
+
+    const filme = {
+        titulo: titulo,
+        genero: genero,
+        status: status,
+        nota: nota
+    };
+
+    console.log(filme);
+
+    $.ajax({
+        url: "http://localhost:3000/filmes",
+        method: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(filme),
+
+        success: function(resposta) {
+            console.log("Filme enviado com sucesso!");
+            console.log(resposta);
+        },
+
+        error: function(erro) {
+            console.log("Erro ao enviar filme");
+            console.log(erro);
+        }
+    });
+
+});
