7607fd622eb8c9ec922aa5e6426a32f28761d59b
Dudb25 <edudomingos0@gmail.com>
2026-05-26 22:42:13 -0300

    commit para troca de branch

--- backend/routers/filmes.py
+++ backend/routers/filmes.py
@@ -1,7 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from backend.database import SessionLocal
+from backend.database import SessionLocal, get_db
 from backend import models, schemas
 
 router = APIRouter(prefix="/filmes", tags=["Filmes"])
+
+@router.post("/", response_model=schemas.Filme)
+def criar_filme(filme: schemas.FilmeCreate, db: Session = Depends(get_db)):
+    db_filme = models.Filme(**filme.dict())
+    db.add(db_filme)
+    db.commit()
+    db.refresh(db_filme)
+    return db_filme
