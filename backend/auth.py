66400a9875b1eb02935339016276c6ca98d6fe69
Dudb25 <edudomingos0@gmail.com>
2026-06-15 21:52:43 -0300

    protegendo rotas com token

--- backend/auth.py
+++ backend/auth.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import HTTPException, status, Depends
 
 SECRET_KEY = "chave_secreta"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -15,3 +18,17 @@ def create_access_token(data: dict):
     enconded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return enconded_jwt
+
+def verificar_token(token: str = Depends(oauth2_scheme)):
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload.get("sub")
+
+        if email is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token inválido")
+        
+        return payload
+    
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token inválido ou expirado")
