ae70e5e248fb5cf024d27c25374fa065357dbf91
Dudb25 <edudomingos0@gmail.com>
2026-06-29 22:57:56 -0300

    cadastro de usuario

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from backend.database import get_db
 from backend import models, schemas
@@ -6,13 +6,16 @@ from backend.auth import hash_password, verificar_token
 
 router = APIRouter(prefix="/usuarios", tags=["Usuários"])
 
-@router.post("/", response_model=schemas.UsuarioResponse)
-def criar_usuario(usuario: schemas.UsuarioCreate, db: Session = Depends(get_db), usuario_logado = Depends(verificar_token)):
+@router.post("", response_model=schemas.UsuarioResponse, status_code=status.HTTP_201_CREATED)
+def criar_usuario(usuario: schemas.UsuarioCreate, db: Session = Depends(get_db)):
 
     db_usuario = db.query(models.Usuario).filter(models.Usuario.email == usuario.email).first()
 
     if db_usuario:
-        raise HTTPException(status_code=400, detail="Email já cadastrado")
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Email ja esta em uso"
+        )
     
     novo_usuario = models.Usuario(
         nome = usuario.nome,
